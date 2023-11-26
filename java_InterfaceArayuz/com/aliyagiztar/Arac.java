bc5e22c40bd416a0c5644ec8c7e533725d3dc1b3
aliyagiztar <aliyagiz.tar@gmail.com>
2023-11-26 18:15:11 +0300

    interface

--- java_InterfaceArayuz/com/aliyagiztar/Arac.java
+++ java_InterfaceArayuz/com/aliyagiztar/Arac.java
@@ -1,9 +1,27 @@
 package java_InterfaceArayuz.com.aliyagiztar;
 
-public class Arac {
+public class Otobus extends Arac {
+    // Sınıf özellikleri
+    private String plakaNumarasi;
+    private short modelYili;
+    private String modelseri;
+    private boolean sunucuDurumu;
+
     public static void main(String[] args) {
-
-       private  String plakaNumarası;
-
+        // Otobus sınıfından bir nesne oluşturma ve bazı işlemler yapma
+        Otobus otobus = new Otobus();
+        // otobus ile ilgili işlemler burada yapılabilir
     }
+
+    // Otobus sınıfı için getter ve setter metodları eklenebilir
+    // Örnek:
+    public String getPlakaNumarasi() {
+        return plakaNumarasi;
+    }
+
+    public void setPlakaNumarasi(String plakaNumarasi) {
+        this.plakaNumarasi = plakaNumarasi;
+    }
+
+    // Diğer getter ve setter metodları...
 }
