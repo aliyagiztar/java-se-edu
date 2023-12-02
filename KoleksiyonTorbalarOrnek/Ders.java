da7ddf9a5b5b5906611840e5eb0334905ef19fea
aliyagiztar <aliyagiz.tar@gmail.com>
2023-12-03 01:20:07 +0300

    Thread

---
+++ KoleksiyonTorbalarOrnek/Ders.java
@@ -0,0 +1,4 @@
+package KoleksiyonTorbalarOrnek;
+
+public class Ders {
+}
---
+++ KoleksiyonTorbalarOrnek/KoleksiyonlarOrnek.java
@@ -0,0 +1,16 @@
+package KoleksiyonTorbalarOrnek;
+@import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgumentucter
+@AllargumentConstructer
+
+public class KoleksiyonlarOrnek {
+    public static void main(String[] args) {
+
+
+
+    }
+}
