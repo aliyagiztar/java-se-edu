b5f8d9a7db8e2f0612f322f377ecb6bbeb33b6c4
Ali Yağız Tar <aliyagiz.tar@gmail.com>
2024-02-26 14:43:17 +0300

    DevOps Pipeline
    DevOps Pipeline

---
+++ Java_045_DevOpsPipeline/src/test/java/com/mimaraslan/Java045DevOpsPipelineApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mimaraslan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Java045DevOpsPipelineApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
